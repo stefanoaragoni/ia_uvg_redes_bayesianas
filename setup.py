6bd28dc424e46f90f9aca9c87e0f3fcb1a74421b
stefanoaragoni <69864405+stefanoaragoni@users.noreply.github.com>
2023-02-04 11:04:17 -0600

    INIT

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ia_uvg_redes_bayesianas',
+      version='0.0',
+      description='Redes Bayesianas - Universidad del Valle de Guatemala',
+      packages=['ia_uvg_redes_bayesianas'],
+      author_email='ara20261@uvg.edu.gt',
+      zip_safe=False)
