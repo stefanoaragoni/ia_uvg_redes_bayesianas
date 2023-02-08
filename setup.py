b754f46d8978e4f058aaadbcec27c56c283ca92f
Ldsc2002 <luissantoscuellar@gmail.com>
2023-02-07 18:00:17 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ia_uvg_redes_bayesianas',
-      version='1.0',
+      version='1.1',
       description='Redes Bayesianas - Universidad del Valle de Guatemala',
       packages=['ia_uvg_redes_bayesianas'],
       author_email='san20226@uvg.edu.gt',
